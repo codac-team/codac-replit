e14d4f39dde23443ffa548d9745090fd282db685
SimonRohou <simon.rohou@gmail.com>
2020-06-03 20:19:55 +0200

    added delay at first use

--- vibes/vibes_replit.py
+++ vibes/vibes_replit.py
@@ -1,11 +1,17 @@
 import os
 
 def init():
+  if os.environ.get('Foo') is None:
+    os.environ["XDG_RUNTIME_DIR"] = "/tmp/runtime-runner" # avoids a Qt warning
+    added_delay_at_first_use=2
+  else:
+    added_delay_at_first_use=0
+    
   import pygame
   pygame.init()
   os.system('chmod +x vibes/VIBes-viewer && ./vibes/VIBes-viewer &')
   import time
-  time.sleep(1) # waiting for VIBes to be ready
+  time.sleep(1+added_delay_at_first_use) # waiting for VIBes to be ready
   os.system('clear') # removing prior warnings in the terminal
 
 def pauseDrawing():
