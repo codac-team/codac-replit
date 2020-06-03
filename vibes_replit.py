1491c5c36fb9f43468a3f2fa49ca57edeecb1270
SimonRohou <simon.rohou@gmail.com>
2020-06-03 18:54:21 +0200

    VIBes stored in the repo

--- vibes_replit.py
+++ vibes_replit.py
@@ -1,12 +1,11 @@
 import os
 
 def init():
-  os.system('[ ! -d squashfs-root ] && wget https://github.com/ENSTABretagneRobotics/VIBES/releases/download/0.2.3/VIBes-0.2.3-linux.AppImage && chmod +x ./VIBes-0.2.3-linux.AppImage && ./VIBes-0.2.3-linux.AppImage --appimage-extract')
-  os.system('cd squashfs-root && chmod +x ./VIBes-viewer && ./VIBes-viewer &')
+  os.system('chmod +x VIBes-viewer && ./VIBes-viewer &')
   import time
-  time.sleep(1) # needed for finalizing the previous installation
-  os.system('clear')
+  time.sleep(1) # waiting for VIBes to be ready
+  os.system('clear') # removing prior Qt warnings in the terminal
 
 def pauseDrawing():
   import sys
-  sys.stdin.read(1) # without that the GUI closes...
+  sys.stdin.read(1) # prevent the GUI closing...
