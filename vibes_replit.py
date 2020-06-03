71210818ae85cc7bf6a0ea7d178d6b5d3c4cf8a7
SimonRohou <simon.rohou@gmail.com>
2020-06-03 19:06:35 +0200

    Configure X with pygame

--- vibes_replit.py
+++ vibes_replit.py
@@ -1,10 +1,12 @@
 import os
 
 def init():
+  import pygame
+  pygame.init()
   os.system('chmod +x VIBes-viewer && ./VIBes-viewer &')
   import time
   time.sleep(1) # waiting for VIBes to be ready
-  os.system('clear') # removing prior Qt warnings in the terminal
+  os.system('clear') # removing prior warnings in the terminal
 
 def pauseDrawing():
   import sys
